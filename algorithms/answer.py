371497f7d1474f56041d2358c2b4f90b5ad25536
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2024-12-26 01:45:18 +0800

    new answer function

--- algorithms/answer.py
+++ algorithms/answer.py
@@ -5,14 +5,7 @@ import numpy as np
 MINIMUM_CONFIDENCE = float(getenv("MINIMUM_CONFIDENCE") or "0.3")
 
 def model_predict_answer(model_predict: tuple[Response, ...]) -> list[int]:
-    model_prediction_label_confidence = map(lambda x: x.label_confidence, model_predict)
-    model_prediction_result = [np.sum(t)/len(model_predict) for t in zip(*model_prediction_label_confidence)]
-    model_prediction_reply = list(map(
-        lambda x: x[0],
-        filter(
-            lambda x: x[1] > MINIMUM_CONFIDENCE,
-            enumerate(model_prediction_result)
-        )
-    ))
+    model_prediction_label_confidence = map(lambda x: 10 ** np.argsort(list(x.label_confidence)), model_predict)
+    model_prediction_result = [np.sum(t) for t in zip(*model_prediction_label_confidence)]
 
-    return model_prediction_reply
+    return model_prediction_result[-1]
