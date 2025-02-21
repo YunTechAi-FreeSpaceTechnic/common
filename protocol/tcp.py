574336c52e5227483066a5e33003deac3a3ecdd7
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2025-02-21 18:15:03 +0800

    fix bufsize overflow

--- protocol/tcp.py
+++ protocol/tcp.py
@@ -5,8 +5,14 @@ from asyncio import AbstractEventLoop
 from typing import List
 from logging import Logger
 
+SOCKET_BUFFET_SIZE = 8192
+
+
 class TCPServer():
-    def __init__(self, logger: Logger, host: str = "0.0.0.0",  port = 6666, callback: Callable[[bytes], bytes] = lambda x: x):
+    def __init__(
+            self, logger: Logger, host: str = "0.0.0.0",
+            port=6666, callback: Callable[[bytes], bytes] = lambda x: x
+    ):
         self.port = port
         self.host = host
         self.calback = callback
@@ -25,7 +31,10 @@ class TCPServer():
         while True:
             try:
                 conn, addr = self.socket.accept()
-                connection = Connection(self.logger, conn, addr, lambda: self.connections.remove(connection), self.calback)
+                connection = Connection(
+                    self.logger, conn, addr,
+                    lambda: self.connections.remove(connection), self.calback
+                )
                 self.connections.append(connection)
             except:
                 pass
@@ -36,8 +45,11 @@ class TCPServer():
             connection.thread.join()
         self.socket.close()
 
+
 class Connection():
-    def __init__(self, logger: Logger, conn: SocketType, addr, on_close: Callable, callback: Callable[[bytes], bytes]):
+    def __init__(self, logger: Logger, conn: SocketType, addr,
+                 on_close: Callable, callback: Callable[[bytes], bytes]
+                 ):
         self.logger = logger
         self.conn = conn
         self.addr = addr
@@ -55,16 +67,23 @@ class Connection():
 
     def recv(self):
         size = self.conn.recv(4)
+        buf_bytes = bytearray()
         self.logger.debug(f"Server received size: {size}")
 
         if not size:
             return None
+        size = int.from_bytes(size)
 
-        return self.conn.recv(int.from_bytes(size))
+        for i in range(int(size / SOCKET_BUFFET_SIZE)):
+            buf_bytes.extend(self.conn.recv(SOCKET_BUFFET_SIZE))
+
+        buf_bytes.extend(self.conn.recv(size % SOCKET_BUFFET_SIZE))
+
+        return buf_bytes
+
     def close(self):
         self.conn.close()
         self.on_close()
-
 
     def listen(self):
         data = self.recv()
@@ -83,8 +102,9 @@ class Connection():
             self.on_close()
             return
 
+
 class TCPClient():
-    def __init__(self, loop: AbstractEventLoop, host = "localhost", port = 6666):
+    def __init__(self, loop: AbstractEventLoop, host="localhost", port=6666):
         self.loop = loop
         self.host = host
         self.port = port
@@ -93,18 +113,28 @@ class TCPClient():
 
     async def send(self, data: bytes) -> bytes | None:
         size = len(data)
-
         await self.loop.sock_sendall(self.socket, size.to_bytes(4) + data)
 
         return await self.recv()
 
     async def recv(self):
         size = await self.loop.sock_recv(self.socket, 4)
+        buf_bytes = bytearray()
 
         if not size:
             return None
 
-        return await self.loop.sock_recv(self.socket, int.from_bytes(size))
+        size = int.from_bytes(size)
+
+        for i in range(int(size / SOCKET_BUFFET_SIZE)):
+            buf_bytes.extend(await self.loop.sock_recv(
+                self.socket, SOCKET_BUFFET_SIZE))
+
+        buf_bytes.extend(await self.loop.sock_recv(
+            self.socket, SOCKET_BUFFET_SIZE
+        ))
+
+        return buf_bytes
 
     def close(self):
         self.socket.close()
--- tests/test_tcp.py
+++ tests/test_tcp.py
@@ -1,4 +1,6 @@
 from protocol.tcp import TCPServer, TCPClient
+from protocol.byte_buffter import ByteBuffter
+from ModelAPI import Request, Text
 import pytest
 import asyncio
 import logging
@@ -8,11 +10,15 @@ pytest_plugins = ('pytest_asyncio',)
 
 @pytest.mark.asyncio
 async def test_tcp_server():
-    test_data = b"Hello World"
+    test_data = Request("abc", [Text("ab", "") for i in range(10000)])
+    buf = ByteBuffter()
+    test_data.encode(buf)
     server = TCPServer(logging)
     server.listen_thread.start()
-    await asyncio.sleep(1)
-    client = TCPClient(asyncio.get_event_loop())
-    data = await client.send(test_data)
 
-    assert data == test_data
+    await asyncio.sleep(1)
+
+    client = TCPClient(asyncio.get_event_loop())
+    data = await client.send(buf.to_bytes())
+
+    assert Request.decode(ByteBuffter(data)) == test_data
